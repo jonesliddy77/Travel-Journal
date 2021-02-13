4fdf9c859443d77138698b6f4e7a94e9d724d361
jonesliddy77 <jonesliddy77@gmail.com>
2021-02-13 13:27:36 -0600

    adding files

--- config/config.js
+++ config/config.js
@@ -1,13 +1,14 @@
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": "traveljournal_db",
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
-  },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
-  }
+    development: {
+        username: process.env.DB_USER,
+        password: 'jonesliddy77',
+        database: 'traveljournal_db',
+        port:3306,
+        host: "localhost",
+        dialect: 'mysql',
+    },
+    production: {
+        use_env_variable: 'JAWSDB_URL',
+        dialect: 'mysql',
+    },
 };
